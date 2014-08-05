f7640b4bbe14a39b16f65b282b2442b724260dfa
sbaresel <sbaresel@172.23.3.177>
2014-08-05 13:01:33 +0200

    Stand August

--- src/systemcontrolmanager/SystemControlManager.java
+++ src/systemcontrolmanager/SystemControlManager.java
@@ -21,6 +21,7 @@ import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.naming.NamingException;
@@ -43,27 +44,27 @@ public class SystemControlManager {
             String from = props.getProperty("mail.from");
             Class.forName("org.postgresql.Driver");
             String temp="LKHduguge";
-            /* Get Mail Configuration */
-            
+            /* Allocate Variables */
             String hostM = null; int portM = 0; String userM = null; String passM = null; String footM = null;
-                
-            Connection cnM = DriverManager.getConnection(props.getProperty("url.repository"));
-            PreparedStatement psM = cnM.prepareStatement("SELECT decode(key,'base64'),decode(val,'base64') FROM config_gateway WHERE MOD like encode('MAILAPI','base64') ORDER BY 1 ASC");
-            ResultSet rsM = psM.executeQuery();
-            while ( rsM.next() ) { 
-                if ("HOST".equals(rsM.getString( 1 ))) {
-                    hostM = rsM.getString( 2 );
-                } else if ("PORT".equals(rsM.getString( 1 ))) {
-                    portM = Integer.parseInt(rsM.getString( 2 ));
-                } else if ("USER".equals(rsM.getString( 1 ))) {
-                    userM = rsM.getString( 2 );
-                } else if ("PASS".equals(rsM.getString( 1 ))) {
-                    passM = rsM.getString( 2 );
-                } else if ("FOOTER".equals(rsM.getString( 1 ))) {
-                    footM = rsM.getString( 2 );
+            /* Open JDBC Connections */
+            Connection cn = DriverManager.getConnection(props.getProperty("url.repository"));
+            /* Get Mail Configuration */
+            PreparedStatement ps = cn.prepareStatement("SELECT decode(key,'base64'),decode(val,'base64') FROM config_gateway WHERE MOD like encode('MAILAPI','base64') ORDER BY 1 ASC");
+            ResultSet rs = ps.executeQuery();
+            while ( rs.next() ) { 
+                if ("HOST".equals(rs.getString( 1 ))) {
+                    hostM = rs.getString( 2 );
+                } else if ("PORT".equals(rs.getString( 1 ))) {
+                    portM = Integer.parseInt(rs.getString( 2 ));
+                } else if ("USER".equals(rs.getString( 1 ))) {
+                    userM = rs.getString( 2 );
+                } else if ("PASS".equals(rs.getString( 1 ))) {
+                    passM = rs.getString( 2 );
+                } else if ("FOOTER".equals(rs.getString( 1 ))) {
+                    footM = rs.getString( 2 );
                 }
             } 
-            cnM.close();
+            cn.close();
             
             Integer i = 0;
             Long ctime1 = System.currentTimeMillis()/1000;
@@ -83,30 +84,33 @@ public class SystemControlManager {
                 log.write("[" + ctime1 + "] Konfiguration: mail.user = " + userM + "\n");
                 log.write("[" + ctime1 + "] Konfiguration: mail.pass = " + passM + "\n");
                 log.write("[" + ctime1 + "] Konfiguration: mail.from = " + props.getProperty("mail.from") + "\n");
+                log.write("[" + ctime1 + "] Konfiguration: mail.admin = " + props.getProperty("mail.admin") + "\n");
                 log.write("[" + ctime1 + "] Dienst: Loop gestartet\n");
                 log.flush();
             } else {
                 log.close();
             }
             
+            
+        if ( isValidEmailAddress(props.getProperty("mail.from")) && isValidEmailAddress(props.getProperty("mail.admin")) ) {
+                
             while (true) {
+                /* Open JDBC Connections */
+                Connection cnM = DriverManager.getConnection(props.getProperty("url.monitoring"));
+                Connection cnR = DriverManager.getConnection(props.getProperty("url.repository"));
+                /* Select Monitoring Mails */
                 Long ctime2 = System.currentTimeMillis()/1000;
-                Connection cn = DriverManager.getConnection(props.getProperty("url.monitoring"));
-                PreparedStatement psLt = cn.prepareStatement("select b.hstln,d.srvna,c.instna,e.rlid,decode(a.usr,'base64'),decode(a.comment,'base64'),f.mtyph,f.mtypt,a.created,a.mailid,g.current_state,a.t1,a.t2,f.mtypid from monitoring_mailing a, monitoring_info_host b, monitoring_info_instance c, monitoring_info_service d, monitoring_host_role_mapping e,class_mailtypes f, monitoring_status g where a.hstid=b.hstid and b.instid=c.instid and a.srvid=d.srvid and a.hstid=e.hstid and a.mtypid=f.mtypid and a.srvid=g.srvid and a.done=false");
+                PreparedStatement psLt = cnM.prepareStatement("select b.hstln,d.srvna,c.instna,e.rlid,decode(a.usr,'base64'),decode(a.comment,'base64'),f.mtyph,f.mtypt,a.created,a.mailid,g.current_state,a.t1,a.t2,f.mtypid from monitoring_mailing a, monitoring_info_host b, monitoring_info_instance c, monitoring_info_service d, monitoring_host_role_mapping e,class_mailtypes f, monitoring_status g where a.hstid=b.hstid and b.instid=c.instid and a.srvid=d.srvid and a.hstid=e.hstid and a.mtypid=f.mtypid and a.srvid=g.srvid and a.done=false");
                 ResultSet rsLt = psLt.executeQuery();
                 while ( rsLt.next() ) {
                     /* initial variables */
                     String host = rsLt.getString( 1 ); String serv = rsLt.getString( 2 ); String inst = rsLt.getString( 3 ); Integer role = rsLt.getInt( 4 ); String uuid = rsLt.getString( 5 ); String comment = rsLt.getString( 6 ); String header = rsLt.getString( 7 ); String text = rsLt.getString( 8 ); long ts = rsLt.getLong( 9 ); String sts = rsLt.getString( 9 ); String user = null; String to = ""; Integer mailid = rsLt.getInt( 10 ); Integer cstate = rsLt.getInt( 11 ); Long t1 = rsLt.getLong( 12 ); Long t2 = rsLt.getLong( 13 ); Integer mtypid = rsLt.getInt( 14 );
-                    /* check if double */
                     /* get full username */
-                    Connection cnR = DriverManager.getConnection(props.getProperty("url.repository"));
                     if (!"system".equals(uuid)) {
                         PreparedStatement psR = cnR.prepareStatement("select decode(usdc,'base64') from profiles_user where usnm=?");
                         psR.setString(1,encodeString( uuid ));
                         ResultSet rsR = psR.executeQuery();
-                        if (rsR.next()) {
-                            user = rsR.getString( 1 );
-                        }
+                        if (rsR.next()) { user = rsR.getString( 1 ); }
                     } else {
                         user = "System";
                     }
@@ -116,7 +120,23 @@ public class SystemControlManager {
                     ResultSet rsMR = psMR.executeQuery();
                     while (rsMR.next()) {
                         if (!"-".equals(rsMR.getString( 1 ))) {
-                            to+= rsMR.getString( 1 ) + ",";
+                            if (isValidEmailAddress(rsMR.getString( 1 ))) {
+                                to+= rsMR.getString( 1 ) + ",";
+                            } else {
+                                try {
+                                    send(hostM,portM,userM,passM,props.getProperty("mail.admin"),"",from,"++ SystemControlManager ++","<html><head></head><body>Die angegebene E-Mail Adresse ist nicht valide.<br>E-Mail:" + rsMR.getString( 1 ) + "</body></html>");
+                                } catch (NoSuchProviderException ex) {
+                                    Logger.getLogger(SystemControlManager.class.getName()).log(Level.SEVERE, null, ex);
+                                } catch (MessagingException ex) {
+                                    Logger.getLogger(SystemControlManager.class.getName()).log(Level.SEVERE, null, ex);
+                                } catch (NamingException ex) {
+                                    Logger.getLogger(SystemControlManager.class.getName()).log(Level.SEVERE, null, ex);
+                                }
+                                if ("on".equals(props.getProperty("debug"))) {
+                                    log.write("[" + ctime2 + "] ++ SystemControlManager ++ Die angegebene E-Mail Adresse ist nicht valide: " + rsMR.getString( 1 ) + "\n");
+                                    log.flush();
+                                }
+                            }
                         } 
                     }
                     if (to.length() > 0) {
@@ -127,17 +147,17 @@ public class SystemControlManager {
                         String mailsubject = header.replace("_S_", serv).replace("_H_", host).replace("_U_", user).replace("_A_", status);
                         String mailtext;
                         if (mtypid == 2 || mtypid == 6) { 
-                            mailtext = encodeHtml("<font face='Arial' size=2>" + text + "<br><br><hr><b>Instanz:</b> " + inst + "<b><br>Host:</b> " + host + "<br><b>Service:</b> " + serv + "<br><b>Bearbeiter:</b> " + user + "<br><b>Beginn:</b> " + ConvertUtime(t1) + "<br><b>Ende:</b> " + ConvertUtime(t2) + "<hr><br>" + comment + "<br><br>" + footM );
+                            mailtext = encodeHtml("<html><head></head><body><font face='Arial' size=2>" + text + "<br><br><hr><b>Instanz:</b> " + inst + "<b><br>Host:</b> " + host + "<br><b>Service:</b> " + serv + "<br><b>Bearbeiter:</b> " + user + "<br><b>Beginn:</b> " + ConvertUtime(t1) + "<br><b>Ende:</b> " + ConvertUtime(t2) + "<hr><br>" + comment + "<br><br>" + footM + "</font></body></html>");
+                        } else if (mtypid == 1) {
+                            mailtext = encodeHtml("<html><head></head><body><font face='Arial' size=2>" + text + "<br><br><hr><b>Instanz:</b> " + inst + "<b><br>Bearbeiter:</b> " + user + "<hr><br>" + comment + "<br><br>" + footM + "</font></body></html>");
                         } else {
-                            mailtext = encodeHtml("<font face='Arial' size=2>" + text + "<br><br><hr><b>Instanz:</b> " + inst + "<b><br>Host:</b> " + host + "<br><b>Service:</b> " + serv + "<br><b>Bearbeiter:</b> " + user + "<hr><br>" + comment + "<br><br>" + footM );
+                            mailtext = encodeHtml("<html><head></head><body><font face='Arial' size=2>" + text + "<br><br><hr><b>Instanz:</b> " + inst + "<b><br>Host:</b> " + host + "<br><b>Service:</b> " + serv + "<br><b>Bearbeiter:</b> " + user + "<hr><br>" + comment + "<br><br>" + footM + "</font></body></html>");
                         }
                         
                         try {
                             /* send mail */
                             String exec = encodeString("" + mailsubject + "-" + mailtext + "-" + to + "");
-                            if( !temp.equals(exec) ) {
-                                send(hostM,portM,userM,passM,to,"",from,encodeSubject(mailsubject),mailtext);
-                            }
+                            if( !temp.equals(exec) ) { send(hostM,portM,userM,passM,to,"",from,encodeSubject(mailsubject),mailtext); }
                             temp = exec;
                         } catch (NoSuchProviderException ex) {
                             Logger.getLogger(SystemControlManager.class.getName()).log(Level.SEVERE, null, ex);
@@ -152,19 +172,85 @@ public class SystemControlManager {
                             log.flush();
                         }
                     }
-                    cnR.close();
                     /* set false to true */
-                    PreparedStatement psD = cn.prepareStatement("UPDATE monitoring_mailing SET DONE=true WHERE mailid=?");
+                    PreparedStatement psD = cnM.prepareStatement("UPDATE monitoring_mailing SET DONE=true WHERE mailid=?");
                     psD.setInt(1, mailid);
                     psD.executeUpdate();
-                    /* Fill temp array with current values */
                 }
-                /* Close Connection */
-                cn.close();
+                /* Profiles Mailing */
+                PreparedStatement psPM = cnR.prepareStatement("select a.mailid,decode(a.mto,'base64'),decode(a.mcc,'base64'),decode(b.umai,'base64'),decode(a.msubject,'base64'),decode(a.mbody,'base64') from profiles_mailing a, profiles_user b where a.uuid=b.uuid and a.done=false");
+                ResultSet rsPM = psPM.executeQuery();
+                while ( rsPM.next() ) {
+                    /* initial variables */
+                    Integer rmailid = rsPM.getInt( 1 );
+                    String rto = rsPM.getString( 2 );
+                    String rcc = rsPM.getString( 3 );
+                    String rfrom = rsPM.getString( 4 );
+                    String rsubject = encodeSubject( rsPM.getString( 5 ) );
+                    String rtext = encodeHtml( "<html><head></head><body><font face=Arial size=2>" + rsPM.getString( 6 ) + "</font></body></html>");
+                    try {
+                        /* send mail */
+                        send(hostM,portM,userM,passM,rto,rcc,rfrom,rsubject,rtext);                      
+                    } catch (NoSuchProviderException ex) {
+                        Logger.getLogger(SystemControlManager.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (MessagingException ex) {
+                        Logger.getLogger(SystemControlManager.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (NamingException ex) {
+                        Logger.getLogger(SystemControlManager.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    /* debug */
+                    if ("on".equals(props.getProperty("debug"))) {
+                        log.write("[" + ctime2 + "] Loop: \"MAILFROM\":\"" + rfrom + "\",\"MAILTO\":\"" + rto + "\",\"MAILSUBJECT\":\"" + rsubject + ",\"MAILTEXT\":\"" + rtext + "\"\n");
+                        log.flush();
+                    }
+                    /* set false to true */
+                    PreparedStatement psPMD = cnR.prepareStatement("UPDATE profiles_mailing SET DONE=true WHERE mailid=?");
+                    psPMD.setInt(1, rmailid);
+                    psPMD.executeUpdate();
+                }
+                /* Close Connections */
+                /* Monitoring System Mailing */
+                PreparedStatement psMSM = cnM.prepareStatement("select decode(a.usr,'base64'),decode(a.comment,'base64'),b.mtyph,b.mtypt,a.created,a.mailid from monitoring_mailing a, class_mailtypes b where a.hstid=0 and a.srvid=0 and a.mtypid=7 and a.mtypid=b.mtypid and a.done=false");
+                ResultSet rsMSM = psMSM.executeQuery();
+                while ( rsMSM.next() ) {
+                    /* initial variables */
+                    String msmsub = encodeSubject( rsMSM.getString( 3 ).replace(": _M_","") );
+                    String msmtxt = encodeHtml( "<html><head></head><body><font face=Arial size=2>" + rsMSM.getString( 4 ) + "<br><br>" + rsMSM.getString( 2 ) + "</font></body></html>");
+                    Integer msmmailid = rsMSM.getInt( 6 );
+                    try {
+                        /* send mail */
+                        send(hostM,portM,userM,passM,props.getProperty("mail.admin"),"",from,msmsub,msmtxt);                      
+                    } catch (NoSuchProviderException ex) {
+                        Logger.getLogger(SystemControlManager.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (MessagingException ex) {
+                        Logger.getLogger(SystemControlManager.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (NamingException ex) {
+                        Logger.getLogger(SystemControlManager.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    /* debug */
+                    if ("on".equals(props.getProperty("debug"))) {
+                        log.write("[" + ctime2 + "] Loop: \"MAILFROM\":\"" + from + "\",\"MAILTO\":\"" + props.getProperty("mail.admin") + "\",\"MAILSUBJECT\":\"" + msmsub + ",\"MAILTEXT\":\"" + msmtxt + "\"\n");
+                        log.flush();
+                    }
+                    /* set false to true */
+                    PreparedStatement psPMD = cnM.prepareStatement("UPDATE monitoring_mailing SET DONE=true WHERE mailid=?");
+                    psPMD.setInt(1, msmmailid);
+                    psPMD.executeUpdate();
+                }
+                /* Close Connections */
+                cnR.close();
+                cnM.close();
                 Thread.sleep(2000);
                 i++;
             }
-            
+        
+        } else {
+            if ("on".equals(props.getProperty("debug"))) {
+                log.write("[" + ctime1 + "] ++ SystemControlManager ++ Die angegebenen E-Mail Adressen mail.from oder mail.admin sind nicht valide.\n");
+                log.flush();
+            }
+        }
+        
         }
     }
     
@@ -194,6 +280,23 @@ public class SystemControlManager {
         String formattedDate = sdf.format(date);
         sdf.setTimeZone(TimeZone.getTimeZone("GMT-1"));
         return formattedDate;
+    }
+    
+    public static boolean isValidInternetEmailAddress(String email) {
+        boolean result = true;
+        try {
+            InternetAddress emailAddr = new InternetAddress(email);
+            emailAddr.validate();
+        } catch (AddressException ex) {
+            result = false;
+        }
+        return result;
+    }
+    
+    public static boolean isValidEmailAddress(String email) {
+       java.util.regex.Pattern p = java.util.regex.Pattern.compile(".+@.+\\.[a-z]+");
+       java.util.regex.Matcher m = p.matcher(email);
+       return m.matches();
     }
     
     /*
